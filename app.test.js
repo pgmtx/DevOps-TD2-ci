e5ec1fbc4b917e6365ad04ce27f2014484bf8695
pgmtx <programutox@disroot.org>
2026-02-03 17:37:36 +0100

    Ajout de fonction sub

--- app.test.js
+++ app.test.js
@@ -6,3 +6,10 @@ describe("add", () => {
         expect(add(-1, 5)).toBe(4);
     });
 });
+
+describe("sub", () => {
+    test("should return the difference of two numbers", () => {
+        expect(sub(1, 2)).toBe(-1);
+        expect(sub(-1, 5)).toBe(-6);
+    });
+});
