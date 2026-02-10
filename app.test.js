5853df78a462d19541d7236e1ef752da8619aae7
pgmtx <programutox@disroot.org>
2026-02-10 10:11:02 +0100

    Reformatage des fichiers et ajout de paquets pour valider le code html et js en CI/CD

--- app.test.js
+++ app.test.js
@@ -1,15 +1,15 @@
 const { add, sub } = require("./app");
 
 describe("add", () => {
-    test("should return the sum of two numbers", () => {
-        expect(add(1, 2)).toBe(3);
-        expect(add(-1, 5)).toBe(4);
-    });
+	test("should return the sum of two numbers", () => {
+		expect(add(1, 2)).toBe(3);
+		expect(add(-1, 5)).toBe(4);
+	});
 });
 
 describe("sub", () => {
-    test("should return the difference of two numbers", () => {
-        expect(sub(1, 2)).toBe(-1);
-        expect(sub(-1, 5)).toBe(-6);
-    });
+	test("should return the difference of two numbers", () => {
+		expect(sub(1, 2)).toBe(-1);
+		expect(sub(-1, 5)).toBe(-6);
+	});
 });
